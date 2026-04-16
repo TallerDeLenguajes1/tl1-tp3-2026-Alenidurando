c01bba4a45cbb001f6f62f111054d072a81a7a74
Alejandro Durando <ale.durando@outlook.com>
2026-04-16 15:01:59 -0300

    apartado 2 realizado

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -3,11 +3,13 @@
 #include <string.h>
 
 void MostrarPersonas (char* Nombres[]);
+int BuscarNombre(char* Nombres[],char palabra[]);
 
 int main(){
 
     int cantPersonas=5;
     char *Buff;
+    char palabra[20];
     Buff=(char *) malloc(100*sizeof(char));
     char *NombrePersona[5];
     int longNombre;
@@ -20,13 +22,23 @@ int main(){
         longNombre=strlen(Buff);
         NombrePersona[i]= (char *) malloc(longNombre+1*sizeof(char));
         strcpy(NombrePersona[i],Buff);
+        fflush(stdin);
        
     }
     
-    MostrarPersonas (NombrePersona);
+    //MostrarPersonas (NombrePersona);
+    printf("ingresar nombre a buscar: ");
+    fflush(stdin);
+    scanf("%s",&palabra);
 
-
-    getchar();
+        int aux=BuscarNombre(NombrePersona,palabra);
+            if(aux==-1){
+                printf("LA PALABRA NO SE ENCONTRO.");
+            }else printf("\n\tLa palabra encontrada es: %s",NombrePersona[aux]);
+        
+    
+    
+        getchar();
     return 0;
 }
 
@@ -38,4 +50,22 @@ void MostrarPersonas (char* Nombres[]){
 
     }
 
+}
+
+int BuscarNombre(char* Nombres[],char palabra[]){
+
+char *puntero;
+    
+    for (int i = 0; i < 5; i++)
+    {
+        puntero=strstr(Nombres[i],palabra);
+        if(puntero!=NULL){
+           return i;
+        }
+    }
+    
+    if(puntero==NULL){
+        return -1;
+    }
+    
 }
